e82bffb7c0ec5f2df7052c8def776d34c87f4af4
norbertoledo <norbertoledo@gmail.com>
2019-03-03 13:42:24 +0100

    Corrección enviroment port

--- server/config/config.js
+++ server/config/config.js
@@ -11,4 +11,4 @@
  */
 // Si la desplegamos en produccion, el process detecta el puerto,
 // sino es porque estamos en entorno de desarrollo y le asignamos el 3000
- process.env.PORT = process.eventNames.PORT || 3000;
+ process.env.PORT = process.env.PORT || 3000;
