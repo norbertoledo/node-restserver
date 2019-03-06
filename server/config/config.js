12cad31d2b25463fb88a140a3fbfcedb66abba01
norbertoledo <norbertoledo@gmail.com>
2019-03-06 01:59:59 +0100

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -32,7 +32,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else{
-    urlDB = 'mongodb+srv://norbertoledo:iEtwVGdUo0ZnWe4J@cluster0-zd4lm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
     
 }
 
